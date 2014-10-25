057c1feace19edac8b8ff577be7b39b6b161f03e
kjgorman <kieran@kjgorman.com>
2014-10-25 17:50:50 +1300

    [blinddoekt] logging on the server

---
+++ blinddoekt/app/logging.js
@@ -0,0 +1,10 @@
++function () {
+    'use strict'
+
+    function log (req, res, next) {
+        console.log('[%s | %s | %s] %s', new Date(), req.method, req.ip, req.url)
+        next()
+    }
+
+    module.exports = log
+}()
